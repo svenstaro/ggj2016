43ccb6c19b76a8e32a41cf4526a0f07d12a51759
Merlin Rübsteck <merlin@myrdinr.de>
2016-01-30 03:18:16 +0100

    Generate ground randomly with textures from an array

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,21 @@ use opengl_graphics::*;
 use graphics::{ Image, clear, default_draw_state };
 use graphics::rectangle::square;
 use std::path::Path;
+use rand::{Rng, SeedableRng, XorShiftRng};
 
 use cgmath::rad;
 use cgmath::{ Vector2, Vector4 };
 use cgmath::{ Rotation2, Basis2 };
 
-fn draw_background(x: u32, y: u32, context: graphics::context::Context, gl_graphics: &mut GlGraphics, txt: &Texture) {
+fn draw_background(x: u32, y: u32, context: graphics::context::Context, gl_graphics: &mut GlGraphics, textures: &Vec<Texture>, seed: [u32;4]) {
+    let mut rng1: XorShiftRng = SeedableRng::from_seed(seed);
+    let txt: &Texture = textures.get(0).unwrap();
     let (width, height) = txt.get_size();
-    for i in 0..x + 1 {
-        for j in 0..y + 1 {
+    for i in 0..(x/width) + 1 {
+        for j in 0..(y/height) + 1 {
+            let rand = rng1.gen::<u32>() % textures.len() as u32;
+            let txt: &Texture = textures.get(rand as usize).unwrap();
+            let (width, height) = txt.get_size();
             let image = Image::new().rect(square((i * width) as f64, (j * height) as f64, width as f64));
             image.draw(txt, default_draw_state(), context.transform, gl_graphics);
         }
@@ -36,6 +42,8 @@ fn draw_background(x: u32, y: u32, context: graphics::context::Context, gl_graph
 }
 
 fn main() {
+    let mut rng = rand::thread_rng();
+    let seed: [u32;4] = [rng.gen::<u32>(), rng.gen::<u32>(), rng.gen::<u32>(), rng.gen::<u32>()];
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow = WindowSettings::new("GGJ2016", [800, 600])
         .exit_on_esc(true)
@@ -46,11 +54,12 @@ fn main() {
     let mut gl = GlGraphics::new(opengl);
 
     let mut app = app::App::new();
-    let size_image = 160;
     // Add player to entities (player instanciated in app)
     //app.add_entity(Box::new(player::Player::new()));
 
-    let texture = Texture::from_path(Path::new("assets/img/ground/placeholder_01.jpg")).unwrap();
+    let mut textures :Vec<Texture>= Vec::new();
+    textures.push(Texture::from_path(Path::new("assets/img/ground/placeholder_01.jpg")).unwrap());
+    textures.push(Texture::from_path(Path::new("assets/img/ground/placeholder_02.jpg")).unwrap());
 
     for e in window {
         if let Some(args) = e.press_args() {
@@ -62,11 +71,9 @@ fn main() {
         }
 
         if let Some(args) = e.render_args() {
-            let grid_width = args.width / size_image;
-            let grid_height = args.height / size_image;
             gl.draw(args.viewport(), |c, gl| {
                 clear([0.5, 0.2, 0.9, 1.0], gl);
-                draw_background(grid_width, grid_height, c, gl, &texture);
+                draw_background(args.width, args.height, c, gl, &textures, seed);
             });
             app.render(args);
         }
